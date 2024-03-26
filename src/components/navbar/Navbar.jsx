b71a027afecd37349729c6a41f74852038e519bd
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-03-26 10:49:17 +0100

    implémentation de layout

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <div>
+      {/* <ul>
+        <li>
+          <NavLink>Order Online</NavLink>
+        </li>
+        <li>
+          <NavLink>Local Pickup</NavLink>
+        </li>
+        <li>
+          <NavLink>Catering</NavLink>
+        </li>
+        <li>
+          <NavLink>Menu</NavLink>
+        </li>
+        <li>
+          <NavLink>About</NavLink>
+        </li>
+      </ul>
+      <div>
+        <ul>
+          <li>
+            <NavLink>Location </NavLink>
+          </li>
+          <li>
+            <NavLink></NavLink>
+          </li>
+          <button>Order Online</button>
+        </ul>
+      </div> */}
+      navbar
+    </div>
+  );
+};
